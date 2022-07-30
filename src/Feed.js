7a5fbb506cb79db1bb58feed875b740a9e7b74d8
Himanshu <hv65616@gmail.com>
2022-07-30 23:40:20 +0530

    mistake

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./Feed.css"
+import "./Feed.css";
+import TweetBox from "./TweetBox";
 function Feed() {
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
+      <TweetBox></TweetBox>
     </div>
   );
 }
