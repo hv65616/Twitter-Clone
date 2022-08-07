28b1410c87625cb3e832520e280fda3d8a42244e
Himanshu <hv65616@gmail.com>
2022-08-07 17:59:51 +0530

    added the widgets componenet

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import "./Widgets.css"
+import React from "react";
+import "./Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+
 function Widgets() {
   return (
-    <div className='widgets'>
-        <h2>Widgets</h2>
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon"></SearchIcon>
+        <input placeholder="Search Twitter " type="text"></input>
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's Happening?</h2>
+        <TwitterTweetEmbed tweetId={"1302230438128226305"}></TwitterTweetEmbed>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="beWho_U_R"
+          options={{ height: 400 }}
+        ></TwitterTimelineEmbed>
+        <TwitterShareButton
+          url={"https://www.linkedin.com/in/himanshu-verma-94a1a41b8/"}
+        ></TwitterShareButton>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
